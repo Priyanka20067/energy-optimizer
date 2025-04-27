ae8b344a2b36d75fdab3a835749e86122b57c099
Priyanka@20067 <priyankam18042006@gmail.com>
2025-04-27 19:55:29 +0530

    home page are added

---
+++ fb/frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+      <div>
+        <h1>🏠 Welcome to Smart Energy Optimizer</h1>
+        <p>Track, analyze, and optimize your energy usage!</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
