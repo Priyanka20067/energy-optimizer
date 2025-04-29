9a45f1f9174d11badba4935c26a18e78f41666cb
Priyanka@20067 <priyankam18042006@gmail.com>
2025-04-29 15:07:35 +0530

    csv file are used

--- fb/frontend/src/components/DeviceCard.jsx
+++ fb/frontend/src/components/DeviceCard.jsx
@@ -6,6 +6,12 @@ function DeviceCard({ device, usage }) {
   const toggleDevice = () => {
     setIsOn(prev => !prev);
   };
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/api/advice')
+      .then(res => res.json())
+      .then(data => setSuggestions([data.advice]));
+  }, []);
+  
 
   return (
     <div style={{ border: '1px solid gray', padding: '15px', borderRadius: '10px', width: '180px', background: isOn ? '#d1ffd6' : '#ffd1d1' }}>
