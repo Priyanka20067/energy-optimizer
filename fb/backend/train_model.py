d71c664ccbf2037702d236412252c4b6ab52f752
Priyanka@20067 <priyankam18042006@gmail.com>
2025-04-30 15:21:45 +0530

    packages installed

--- fb/backend/train_model.py
+++ fb/backend/train_model.py
@@ -2,15 +2,12 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
-# Load dataset
-df = pd.read_csv('usage.csv')
+df = pd.read_csv("usage.csv")
 X = df[['ac', 'fan', 'light']]
 y = df['advice']
 
-# Train model
 model = RandomForestClassifier()
 model.fit(X, y)
 
-# Save model
-joblib.dump(model, 'ai_model.pkl')
-print("✅ AI model saved as ai_model.pkl")
+joblib.dump(model, "ai_model.pkl")
+print("✅ Model trained and saved as ai_model.pkl")
